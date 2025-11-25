541d3ff365165339e61a20ec1126d28afef98808
DonAlejandro01 <alejandro.perezn@gmail.com>
2025-11-24 21:21:00 -0300

    Subir Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RecordandoJS-03
 Tercer ejercicio para medir mi programacion
+
+## Ejercicio
+
+Escribe una funcion que reciba un arregle de palabras y retorne cuantas palabras tienen 5 o mas caracteres.
+- Sin ocupar Filter
+- Solo con For, if,.length y contadores
+- Debo retornar el numero, un texto
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-//Primero tenemos que ingresar por parametro el array de letras que vamos a ocupar
 function contarLetras(Palabra){
     let contar =0;
-// Ahora tenemos que recorrer el array de datos con for
+
 for(let i=0;i<Palabra.length;i++){
-    //Ahora dentro de cada una de estas palabras debemos contar uno por uno.... ¿Como?
     if(Palabra[i].length>=5){
         contar+=1;
     }
